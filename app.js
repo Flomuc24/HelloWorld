4a2b48fce7bcb8ad8d1808fa5d56960cc2ba8afb
Landsberger Florian <d444451@val-008007.burda.com>
2020-08-24 15:31:02 +0200

    Variable hinzugefügt

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+let beispiel = 5
+beispiel = beispiel + 1;
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!' + beispiel)
 })
 
 app.listen(port, () => {
